bfe103e3685803d98956eb1813ca5542d85f7918
madheshiyakajal <117758679+madheshiyakajal@users.noreply.github.com>
2023-04-28 00:32:01 +0530

    Update Navbar.jsx

--- frontend/src/components/main/Navbar.jsx
+++ frontend/src/components/main/Navbar.jsx
@@ -1,62 +1,52 @@
 import React from 'react'
-import { SiGotomeeting } from 'react-icons/si';
-import './navbar.css'
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    
-    <>
-    {/* Just an image */}
-    <nav className="navbar navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-            height={20}
-            alt="MDB Logo"
-            loading="lazy"
-          />
-        </a>
-      </div>
-    </nav>
-  </>
+    <div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#" style={{color:"Red"}}>
+      FlexSpace
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink className="nav-link active" aria-current="page" to="/main/home">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/main/Signup">
+           Signup
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/main/Login">
+            Login
+          </NavLink>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
   
-  //  <section className="navBarSection">
-  //   <header className="header flex">
-  //     <div className="logoDiv">
-  //       <a href="#" className='logo flex'>
-  //         <h1><SiGotomeeting className='icon'/>FlexSpace.</h1>
-  //       </a>
-  //     </div>
 
-  //     <div className="navBar">
-  //       <ul className="navLists flex">
-  //         {/* <li className="navItem">
-  //           <a href="#" className='navLink'>Home</a>
-  //         </li>
-  //         <li className="navItem">
-  //           <a href="#" className='navLink'>Services</a>
-  //         </li>
-  //         <li className="navItem">
-  //           <a href="#" className='navLink'>About Us</a>
-  //         </li>
-  //         <li className="navItem">
-  //           <a href="#" className='navLink'>Contact Us</a>
-  //         </li> */}
-
-  //         <button className="btn">
-  //           <a href="#">+ ADD YOUR SPACE</a>
-  //         </button>
-  //       </ul>
-  //     </div>
-  //   </header>
-  //  </section>
-  //  </>
   )
 }
 
-export default Navbar;
-
-
-
-
+export default Navbar
