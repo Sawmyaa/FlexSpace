94c2ba994f74caed355c9a6681ed1bc321e491eb
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-04-30 20:16:32 +0530

    Update Home.jsx

--- frontend/src/components/main/Home.jsx
+++ frontend/src/components/main/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
 </section>
 
         {/* Service Section */}
-          <h1 className='srv'>SERVICES</h1>
+          {/* <h1 className='srv'>SERVICES</h1> */}
         <div className="row row-cols-1 row-cols-md-4 g-3">
   <div className="col">
     <div className="card h-100">
@@ -158,60 +158,133 @@ const Home = () => {
 </div>
 
       {/* Gallery */}
+      <>
+  {/* Section: About me */}
+  <section className="mb-10">
+    <div className="container">
+      <div className="row gx-0 align-items-center">
+        {/* First column */}
+        <div className="col-lg-6 mb-5 mb-lg-0">
+          <div className="card rounded-7 me-lg-n5">
+            <div className="card-body p-lg-5 shadow-5">
+              <h2 className="fw-bold">
+                <span className="text-primary">FlexSpace</span>
+              </h2>
+              <p className="fw-bold">
+                <em>“Variety of prices and locations at one place.”</em>
+              </p>
+              <p className="text-muted mb-4">
+                Nunc tincidunt vulputate elit. Mauris varius purus malesuada
+                neque iaculis malesuada. Aenean gravida magna orci, non
+                efficitur est porta id. Donec magna diam.
+              </p>
+              <p className="text-muted">
+                Ad, at blanditiis quaerat laborum officia incidunt cupiditate
+                dignissimos voluptates eius aliquid minus praesentium!
+                Perferendis et totam ipsum ex aut earum libero accusamus
+                voluptas quod numquam saepe, consequuntur nihil quia tenetur
+                consequatur. Quis, explicabo deserunt quasi assumenda ea maiores
+                nulla, et dolor saepe praesentium natus error reiciendis
+                voluptas iste. Esse, laudantium ipsum animi, quos voluptatibus
+                atque vero repellat sit eligendi autem maiores quasi cum
+                aperiam. Aperiam rerum culpa accusantium, ducimus deserunt
+                aliquid alias vitae quasi corporis placeat vel maiores explicabo
+                commodi!
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* First column */}
+        {/* Second column */}
+        <div className="col-lg-6 mb-5 mb-lg-0">
+          <img
+            src="https://i.pinimg.com/564x/75/bb/7a/75bb7ad43a5ce65090276c92d976f863.jpg"
+            className="w-100 rounded-7 shadow-4"
+            alt=""
+          />
+        </div>
+        {/* Second column */}
+      </div>
+    </div>
+  </section>
+  {/* Section: About me */}
+</>
 
-  {/* Gallery */}
-  <div className="row">
-  <h1 className='cwr'>TOP COWORKING CITIES</h1>
-    <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
-      <img
-        src="https://t3.ftcdn.net/jpg/02/67/71/90/240_F_267719012_Kfn3ce4iMX5F69Hgk2zR8gzrOnyBOhjZ.jpg"
-        className="w-100 shadow-1-strong rounded mb-4"
-        alt="Boat on Calm Water"
-      />
-      <img
-        src="https://t3.ftcdn.net/jpg/04/01/43/06/240_F_401430617_QU5FmwCbN3oiysrAnVN2yXreAkbPOESe.jpg"
-        className="w-100 shadow-1-strong rounded mb-4"
-        alt="Wintry Mountain Landscape"
-      />
-    </div>
-    <div className="col-lg-4 mb-4 mb-lg-0">
-      <img
-        src="https://i.pinimg.com/564x/e0/6a/e2/e06ae29ad41099ced40dc13352370cae.jpg"
-        className="w-100 shadow-1-strong rounded mb-4"
-        alt="Mountains in the Clouds"
-      />
-      <img
-        src="https://t3.ftcdn.net/jpg/00/50/44/02/240_F_50440279_NjJDH4VfoKnV2jsgXAAonTgWtq5QXKdl.jpg"
-        className="w-100 shadow-1-strong rounded mb-4"
-        alt="Boat on Calm Water"
-      />
-    </div>
-    <div className="col-lg-4 mb-4 mb-lg-0">
-      <img
-        src="https://t3.ftcdn.net/jpg/04/76/78/86/240_F_476788612_cgmTI5EvXqrIR2eyj1yWmTHHUDz17v91.jpg"
-        className="w-100 shadow-1-strong rounded mb-4"
-        alt="Waves at Sea"
-      />
-      <img
-        src="https://t4.ftcdn.net/jpg/05/40/30/71/240_F_540307170_qg8R8Seug9NYJzL7FpbBuW5St0lIyAvv.jpg"
-        className="w-100 shadow-1-strong rounded mb-4"
-        alt="Yosemite National Park"
-      />
+<>
+  {/* First column */}
+  <div className="col-lg-6 mb-5 mb-lg-0">
+    <div
+      style={{
+        background: "hsla(0, 0%, 100%, 0.55)",
+        backdropFilter: "blur(30px)",
+        zIndex: 1
+      }}
+      className="card rounded-7 me-lg-n5"
+    >
+      <div className="card-body p-lg-5 shadow-5">
+        <form>
+          {/* Name input */}
+          <div className="form-outline mb-4">
+            <input type="text" id="form4Example1" className="form-control" />
+            <label className="form-label" htmlFor="form4Example1">
+              Name
+            </label>
+          </div>
+          {/* Email input */}
+          <div className="form-outline mb-4">
+            <input type="email" id="form4Example2" className="form-control" />
+            <label className="form-label" htmlFor="form4Example2">
+              Email address
+            </label>
+          </div>
+          {/* Message input */}
+          <div className="form-outline mb-4">
+            <textarea
+              className="form-control"
+              id="form4Example3"
+              rows={4}
+              defaultValue={""}
+            />
+            <label className="form-label" htmlFor="form4Example3">
+              Message
+            </label>
+          </div>
+          {/* Checkbox */}
+          <div className="form-check d-flex justify-content-center mb-4">
+            <input
+              className="form-check-input me-2"
+              type="checkbox"
+              defaultValue=""
+              id="form4Example4"
+              defaultChecked=""
+            />
+            <label className="form-check-label" htmlFor="form4Example4">
+              Send me a copy of this message
+            </label>
+          </div>
+          {/* Submit button */}
+          <button type="submit" className="btn btn-primary btn-block mb-4">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   </div>
-  {/* Gallery */}
-
-{/* <>
-<video className="vid" src={video} autoPlay loop muted />
-  <h2 className='name'>FLEXSPACE</h2>
-</> */}
-
-
-     </div>
-
-     
-  )
-}
-
-export default Home
+  {/* First column */}
+</>
+<>
+  {/* Second column */}
+  <div className="col-lg-6 mb-5 mb-lg-0">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96813.17497894862!2d-73.97484803586903!3d40.68692922859912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2588f046ee661%3A0xa0b3281fcecc08c!2sManhattan%2C%20New%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2spl!4v1671718528728!5m2!1sen!2spl"
+      width={600}
+      height={450}
+      style={{ border: 0 }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    />
+  </div>
+  {/* Second column */}
+</>
 
