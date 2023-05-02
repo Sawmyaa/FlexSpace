98aa151a51ad07644e5223d4a60e05559be5f087
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-05-02 12:45:20 +0530

    Update Footer.jsx

--- frontend/src/components/main/Footer.jsx
+++ frontend/src/components/main/Footer.jsx
@@ -1,145 +1,259 @@
 import React from 'react'
+import video from '../../Assets/video.mp4'
 
-
-
-
-const Signup = () => {
+const Footer = () => {
   return (
-    <>
-      {/* Section: Design Block */}
-      <section className="text-center">
-        {/* Background image */}
-        <div
-          className="p-5 bg-image"
-          style={{
-            backgroundImage:
-              'url("/Signup3.jpg")',
-            height: "350px",
+    // <section className="footer">
+    //   <div className="videoDiv">
+    //     <video src={video} loop autoPlay muted type="video/mp4"></video>
+    //   </div>
+    // </section>
 
-          }}
-        />
-        {/* Background image */}
-        <div
-          className="card mx-4 mx-md-5 shadow-5-strong"
-          style={{
-            marginTop: "-100px",
-            background: "hsla(0, 0%, 100%, 0.8)",
-
-            backdropFilter: "blur(5px)"
-          }}
+<>
+  {/* Footer */}
+  <footer className="bg-dark text-center text-white">
+    {/* Grid container */}
+    <div className="container p-4">
+      {/* Section: Social media */}
+      <section className="mb-4">
+        {/* Facebook */}
+        <a
+          className="btn btn-outline-light btn-floating m-1"
+          href="#!"
+          role="button"
         >
-          <div className="card-body py-5 px-md-4">
-            <div className="row d-flex justify-content-center">
-              <div className="col-lg-8">
-                <h2 className="fw-bold mb-5">Sign up now</h2>
-                <form>
-                  {/* 2 column grid layout with text inputs for the first and last names */}
-                  <div className="row">
-                    <div className="d-flex flex-row align-items-center mb-4">
-                      <i className="fas fa-user fa-lg me-3 fa-fw" />
-                      <div className="flex-fill mb-0">
-                        <input
-                          type="text"
-                          autoComplete="off"
-                          id="name"
-                          className='form-control'
-                          placeholder='Your Name'
-                        />
-
-                      </div>
-
-                    </div>
-                  </div>
-                  {/* Email input */}
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-envelope fa-lg me-3 fa-fw" />
-                    <div className="flex-fill mb-0">
-                      <input
-                        type="email"
-                        id="email"
-                        className="form-control"
-                        placeholder='Your email'
-                      />
-                    </div>
-                  </div>
-                  {/* Password input */}
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw" />
-                    <div className="flex-fill mb-0">
-                      <input
-                        type="password"
-                        id="password"
-                        className="form-control"
-                        placeholder='Enter password'
-
-                      />
-                    </div>
-                  </div>
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-key fa-lg me-3 fa-fw" />
-                    <div className="flex-fill mb-0">
-                      <input
-                        type="password"
-                        id="cpassword"
-                        className="form-control"
-                        placeholder='Repeat your passward'
-                      />
-                    </div>
-                  </div>
-                  {/* Checkbox */}
-                  <div className="form-check d-flex justify-content-center mb-4">
-                    <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      defaultValue=""
-                      id="form2Example33"
-                      defaultChecked=""
-                    />
-                    <label className="form-check-label" htmlFor="form2Example3">
-                      I agree all statements in{" "}
-                      <a href="#!">Terms of service</a>
-                    </label>
-                  </div>
-                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="submit" style={{ width: "300px" }} className="btn btn-primary btn-lg">
-                      Register
-                    </button>
-                  </div>
-                  {/* Register buttons */}
-                  <div className="text-center">
-                    <p>or sign up with:</p>
-                    <button
-                      type="button"
-                      className="btn btn-link btn-floating mx-1"
-                    >
-                      <i className="fab fa-facebook-f" />
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-link btn-floating mx-1"
-                    >
-                      <i className="fab fa-google" />
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-link btn-floating mx-1"
-                    >
-                      <i className="fab fa-twitter" />
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-link btn-floating mx-1"
-                    >
-                      <i className="fab fa-github" />
-                    </button>
-                  </div>
-                </form>
-
-              </div>
-
+          <i className="fab fa-facebook-f" />
+        </a>
+        {/* Twitter */}
+        <a
+          className="btn btn-outline-light btn-floating m-1"
+          href="#!"
+          role="button"
+        >
+          <i className="fab fa-twitter" />
+        </a>
+        {/* Google */}
+        <a
+          className="btn btn-outline-light btn-floating m-1"
+          href="#!"
+          role="button"
+        >
+          <i className="fab fa-google" />
+        </a>
+        {/* Instagram */}
+        <a
+          className="btn btn-outline-light btn-floating m-1"
+          href="#!"
+          role="button"
+        >
+          <i className="fab fa-instagram" />
+        </a>
+        {/* Linkedin */}
+        <a
+          className="btn btn-outline-light btn-floating m-1"
+          href="#!"
+          role="button"
+        >
+          <i className="fab fa-linkedin-in" />
+        </a>
+        {/* Github */}
+        <a
+          className="btn btn-outline-light btn-floating m-1"
+          href="#!"
+          role="button"
+        >
+          <i className="fab fa-github" />
+        </a>
+      </section>
+      {/* Section: Social media */}
+      {/* Section: Form */}
+      <section className="">
+        <form action="">
+          {/*Grid row*/}
+          <div className="row d-flex justify-content-center">
+            {/*Grid column*/}
+            <div className="col-auto">
+              <p className="pt-2">
+                <strong>Sign up for our newsletter</strong>
+              </p>
             </div>
-
+            {/*Grid column*/}
+            {/*Grid column*/}
+            <div className="col-md-5 col-12">
+              {/* Email input */}
+              <div className="form-outline form-white mb-4">
+                <input
+                  type="email"
+                  id="form5Example21"
+                  className="form-control"
+                />
+                <label className="form-label" htmlFor="form5Example21">
+                  Email address
+                </label>
+              </div>
+            </div>
+            {/*Grid column*/}
+            {/*Grid column*/}
+            <div className="col-auto">
+              {/* Submit button */}
+              <button type="submit" className="btn btn-outline-light mb-4">
+                Subscribe
+              </button>
+            </div>
+            {/*Grid column*/}
           </div>
+          {/*Grid row*/}
+        </form>
+      </section>
+      {/* Section: Form */}
+      {/* Section: Text */}
+      <section className="mb-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          distinctio earum repellat quaerat voluptatibus placeat nam, commodi
+          optio pariatur est quia magnam eum harum corrupti dicta, aliquam sequi
+          voluptate quas.
+        </p>
+      </section>
+      {/* Section: Text */}
+      {/* Section: Links */}
+      <section className="">
+        {/*Grid row*/}
+        <div className="row">
+          {/*Grid column*/}
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="#!" className="text-white">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/*Grid column*/}
+          {/*Grid column*/}
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="#!" className="text-white">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/*Grid column*/}
+          {/*Grid column*/}
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="#!" className="text-white">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/*Grid column*/}
+          {/*Grid column*/}
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="#!" className="text-white">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#!" className="text-white">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/*Grid column*/}
+        </div>
+        {/*Grid row*/}
+      </section>
+      {/* Section: Links */}
+    </div>
+    {/* Grid container */}
+    {/* Copyright */}
+    <div
+      className="text-center p-3"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+    >
+      © 2020 Copyright:
+      <a className="text-white" href="https://mdbootstrap.com/">
+        MDBootstrap.com
+      </a>
+    </div>
+    {/* Copyright */}
+  </footer>
+  {/* Footer */}
+</>
+
+
+  )
+}
+
+export default Footer
 
         </div>
       </section>
