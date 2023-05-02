abe9f515c20f5ed6b0e2653e27ab2764cfe7078b
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-05-02 12:51:33 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Main from './components/main';
 import Home from './components/main/Home';
 import Signup from './components/main/Signup';
 import Login from './components/main/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/main/Navbar';
+import Footer from './components/main/Footer';
+
 
 function App() {
   return (
-    <div>
+  
       <BrowserRouter>
       <Routes>
+        {/* <Route path='/' element={<Navigate to="/Main/Home"/>}> */}
         <Route path="main" element={<Main />} >
           <Route path="home" element={<Home />} />
           <Route path="signup" element={<Signup />} />
           <Route path="login" element={<Login />} />
+          {/* <Route path="footer" element={<Footer />} /> */}
         </Route>
       </Routes>
       </BrowserRouter>
-    </div>
+    
   );
 }
 
