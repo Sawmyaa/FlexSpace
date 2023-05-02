34817d0e6ec04ca1783e0603195c099596a348d9
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-05-02 12:47:57 +0530

    Update index.js

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 
 
 const Main = () => {
   return (
     <div>
+      <Navbar/>
        <Outlet/> {/*this is for child router */}
+       <Footer/>
     </div>
   )
 }
