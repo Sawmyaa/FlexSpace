2f02025b333bc76145dc4708bc9129f73eab00a9
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-05-02 12:42:07 +0530

    Add files via upload

---
+++ backend/models/spaceModel.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require('../connection');
+
+const myschema = new Schema({
+    providerName : String,
+    providerContact : Number,
+    providerEmail : String,
+    name : String,
+    size : String,
+    rate : String,
+    facilities : String,
+    location : String,
+    image : String,
+    user : {type : Types.ObjectId, ref: 'users'}
+});
+
+module.exports = model('space', myschema);
