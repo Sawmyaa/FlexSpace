62363eb3b2047340166828e32be162817666883c
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-05-02 12:50:12 +0530

    Update Navbar.jsx

--- frontend/src/components/main/Navbar.jsx
+++ frontend/src/components/main/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { SiGotomeeting } from 'react-icons/si';
+import './navbar.css'
 import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
   return (
+    
     <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#" style={{color:"Red"}}>
+    <a className="navbar-brand fw-bold" href="#" style={{color:"Blue"}}>
+      {/* <SiGotomeeting className='icon'/> */}
       FlexSpace
     </a>
     <button
@@ -39,13 +43,45 @@ const Navbar = () => {
         </li>
        
       </ul>
+      <button type="submit" className="spacebtn btn-primary btn-block mb-1">
+            + ADD YOUR SPACE
+          </button>
     </div>
   </div>
 </nav>
 </div>
-
   
+  //  <section className="navBarSection">
+  //   <header className="header flex">
+  //     <div className="logoDiv">
+  //       <a href="#" className='logo flex'>
+  //         <h1><SiGotomeeting className='icon'/>FlexSpace.</h1>
+  //       </a>
+  //     </div>
 
+  //     <div className="navBar">
+  //       <ul className="navLists flex">
+  //         {/* <li className="navItem">
+  //           <a href="#" className='navLink'>Home</a>
+  //         </li>
+  //         <li className="navItem">
+  //           <a href="#" className='navLink'>Services</a>
+  //         </li>
+  //         <li className="navItem">
+  //           <a href="#" className='navLink'>About Us</a>
+  //         </li>
+  //         <li className="navItem">
+  //           <a href="#" className='navLink'>Contact Us</a>
+  //         </li> */}
+
+  //         <button className="btn">
+  //           <a href="#">+ ADD YOUR SPACE</a>
+  //         </button>
+  //       </ul>
+  //     </div>
+  //   </header>
+  //  </section>
+  //  </>
   )
 }
 
