cca9c247f2968685c9c9b928f444503c3540893c
madheshiyakajal <117758679+madheshiyakajal@users.noreply.github.com>
2023-05-04 02:52:49 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://SaumyaMaurya:Saumya123@cluster0.eixwr95.mongodb.net/?retryWrites=true&w=majority'
+const url = 'mongodb+srv://madheshiyakajal:Kajal123@cluster0.upiopis.mongodb.net/mydatabase?retryWrites=true&w=majority';
 
 mongoose.connect(url)
 .then((result) => {
     console.log('database connected');
-    
 }).catch((err) => {
     console.error(err);
-    
 });
-
 module.exports = mongoose;
