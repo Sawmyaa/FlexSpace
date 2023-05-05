cbae67ba7a2c00d38e95021323816a70436d787b
madheshiyakajal <117758679+madheshiyakajal@users.noreply.github.com>
2023-05-05 21:48:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
-// import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './components/main/about.css';
+import {BrowserRouter,Navigate, Routes, Route} from 'react-router-dom';
 import Main from './components/main';
 import Home from './components/main/Home';
 import Signup from './components/main/Signup';
 import Login from './components/main/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/main/Navbar';
-import Footer from './components/main/Footer';
+import About from './components/main/About';
+
 
 
 function App() {
   return (
-  
-      <BrowserRouter>
+    <div>
+    <BrowserRouter>
+
       <Routes>
-        {/* <Route path='/' element={<Navigate to="/Main/Home"/>}> */}
-        <Route path="main" element={<Main />} >
-          <Route path="home" element={<Home />} />
+        <Route path="/" element={<Navigate to="/main/home"/>}/>
+        <Route path="main" element={<Main />}>
+
+          <Route path="home" element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
           <Route path="signup" element={<Signup />} />
-          <Route path="login" element={<Login />} />
-          {/* <Route path="footer" element={<Footer />} /> */}
+          <Route path="login" element={<Login/>}/>
+          
         </Route>
       </Routes>
       </BrowserRouter>
-    
+       </div>
   );
 }
 
