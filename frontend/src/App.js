8ed8d2bac3328d16b462f559c9b4d6e320f45600
madheshiyakajal <117758679+madheshiyakajal@users.noreply.github.com>
2023-05-09 14:16:10 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,50 @@
 import './App.css';
 import './components/main/about.css';
-import {BrowserRouter,Navigate, Routes, Route} from 'react-router-dom';
+import './components/main/home.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import Main from './components/main';
 import Home from './components/main/Home';
 import Signup from './components/main/Signup';
 import Login from './components/main/Login';
-import About from './components/main/About';
+import About from './components/main/about';
+import Vendor from './components/vendor';
+import AddSpace from './components/vendor/AddSpace';
+import VendorSignup from './components/vendor/VendorSignup';
+import VendorLogin from './components/vendor/VendorLogin';
+ 
+
+
 
 
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
+      <BrowserRouter>
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/main/home"/>}/>
-        <Route path="main" element={<Main />}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/main/home" />} />
+          <Route path="main" element={<Main />}>
 
-          <Route path="home" element={<Home/>}/>
-          <Route path="about" element={<About/>}/>
-          <Route path="signup" element={<Signup />} />
-          <Route path="login" element={<Login/>}/>
-          
-        </Route>
-      </Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+           
+          </Route>
+
+        </Routes>
+        <Routes>
+          <Route path="vendor" element={<Vendor />}>
+            <Route path="addSpace" element={<AddSpace />} />
+            <Route path="VendorSignup" element={<VendorSignup/>}/>
+            <Route path="VendorLogin" element={<VendorLogin/>}/>
+          </Route>
+        </Routes>
       </BrowserRouter>
-       </div>
+    </div >
   );
 }
 
 export default App;
+
