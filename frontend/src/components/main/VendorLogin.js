1634b602ab1947ef6c3094353b731af2ee460979
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-10 11:40:43 +0530

    ..

---
+++ frontend/src/components/main/VendorLogin.js
@@ -0,0 +1,108 @@
+import { Formik } from 'formik'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import { useVendorContext } from '../../context/VendorProvider'
+
+
+const VLogin = () => {
+
+    const navigate = useNavigate()
+
+    const {loggedIn, setLoggedIn} = useVendorContext();
+
+
+    const loginSubmit = async (formdata, { resetForm, setSubmitting }) => {
+        console.log(formdata)
+
+        setSubmitting(true)
+
+        const res = await fetch('http://localhost:5000/vendor/authenticate', {
+            method: 'POST',
+            body: JSON.stringify(formdata),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(res.status)
+
+        if (res.status === 200) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Login Successful'
+            })
+            setLoggedIn(true)       //to show logout button without page refresh
+            const data = await res.json();
+            console.log(data);
+            sessionStorage.setItem('vendor', JSON.stringify(data));
+            navigate('/vendor/add_space')
+            resetForm();
+        }
+        else if (res.status === 401) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Email or Password is incorrect'
+            })
+        }
+        else {
+            Swal.fire({
+                iicon: 'error',
+                title: 'Error',
+                text: 'Something went wrong'
+            })
+        }
+
+        setTimeout(() => {
+            setSubmitting(false)
+        }, 2000)
+    }
+
+
+
+
+
+    return (
+        <div className='login-bg-image vh-100'>
+
+
+            <div className="login-card-position">
+                <div className="card mx-auto w-50">
+                    <div className="card-body">
+                        <h1 className="card-title text-center">VENDER LOGIN</h1>
+
+
+                        <Formik
+                            initialValues={{ email: "", password: "" }}
+                            onSubmit={loginSubmit}>
+                            {({ values, handleSubmit, handleChange, isSubmitting }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <label>Email Address</label>
+                                    <input required type="email" className="form-control mb-3" value={values.email} name="email" onChange={handleChange} />
+
+                                    <label>Password</label>
+                                    <input required type="password" className="form-control mb-3" value={values.password} name="password" onChange={handleChange} />
+
+                                    <Link to="/forgetpswd" className="">
+                                        Forgot Password
+                                    </Link>
+
+                                    <button type="submit" className="btn btn-success w-100 my-2">
+                                        {isSubmitting ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : "Submit"}
+                                    </button>
+                                </form>
+
+                                
+                            )}
+                        </Formik>
+                        <p className='text-center mt-2'>Don't have account? <Link to='/main/vsignup'>SignIn</Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VLogin
--- frontend/src/components/vendor/AddSpace.js
+++ frontend/src/components/vendor/AddSpace.js
@@ -1,111 +1,175 @@
-import React from 'react'
+import { Formik, useFormik } from 'formik';
+import { MDBInput, MDBTextArea } from 'mdb-react-ui-kit';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const AddSpace = () => {
-  return (
-    <form>
-    {/* 2 column grid layout with text inputs for the first and last names */}
-    <div className="row mb-4">
-      <div className="col">
-        <div className="form-outline">
-          <input type="text" id="form6Example1" className="form-control" />
-          <label className="form-label" htmlFor="form6Example1">
-            First name
-          </label>
+
+    const { spaceid } = useParams()
+
+    const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('vendor')))
+    console.log(currentUser)
+
+    const [imageData, setImageData] = useState(null)
+    const [vendor, setVendor] = useState([])
+
+    const url = 'http://localhost:5000'
+
+
+    const moreDetails = async () => {
+
+        // console.log(id);
+
+        const res = await fetch('http://localhost:5000/vendor/getbyid/' + spaceid)
+        console.log(res.status);
+        if (res.status === 200) {
+            const data = await res.json()
+            setVendor(data)
+            console.log(data);
+        }
+    }
+    const spaceData = async (formdata, { resetForm }) => {
+        formdata.image = imageData;
+        const res = await fetch('http://localhost:5000/addSpace/add', {
+            method: 'POST',
+            body: JSON.stringify(formdata),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+
+        console.log(res.status);
+
+        if (res.status === 200) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Data Added Successfully'
+            })
+            const data = await res.json();
+            console.log(data)
+            resetForm();
+            // return data._id;
+            // navigate('/user');
+        }
+    }
+
+    const uploadFile = (e) => {
+        const file = e.target.files[0];
+        const fd = new FormData();
+        fd.append("myfile", file);
+        fetch(url + "/util/uploadfile", {
+            method: "POST",
+            body: fd,
+        }).then((res) => {
+            if (res.status === 200) {
+                console.log("file uploaded");
+                // console.log(file.name);
+                // console.log(file);
+                setImageData(file.name);
+
+            }
+        });
+    };
+
+    useEffect(() => {
+        moreDetails();
+    }, []);
+
+
+
+
+
+    return (
+        <div className='container'>
+
+            <div className="card border border-primary mt-5">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-md-6 my-auto">
+                            <img src="https://coolwallpapers.me/picsup/3090602-apple_computer_desk_electronics_indoors_lamp_laptop_light_macbook_room_table_technology_workplace_workspace.jpg" alt="" className='w-100' />
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card shadow-0">
+                                <div className="card-body">
+                                    <h3 className="card-title text-center">ADD OFFICE SPACE</h3>
+
+                                    <Formik
+                                        initialValues={{
+                                            providerName: '',
+                                            providerContact: '',
+                                            providerEmail: '',
+                                            name: '',
+                                            size: '',
+                                            rate: '',
+                                            location: '',
+                                            facilities: '',
+                                            image: '',
+                                            vendor: currentUser._id
+                                        }}
+                                        onSubmit={spaceData}>
+
+                                        {({ values, handleChange, handleSubmit }) => (
+                                            <form onSubmit={handleSubmit}>
+
+                                                <div className="row">
+                                                    <div className="col-md-6  mt-3">
+                                                        <MDBInput className='' label="Provider's Name" id='providerName'
+                                                            value={values.providerName} onChange={handleChange} type='text' />
+                                                    </div>
+                                                    <div className="col-md-6 mt-3">
+                                                        <MDBInput className='' label="Provider's Contact" id='providerContact' value={values.providerContact} onChange={handleChange} type='number' />
+                                                    </div>
+                                                </div>
+
+                                                <MDBInput className='mt-3' label="Provider's Email" id='providerEmail'
+                                                    value={values.providerEmail} onChange={handleChange} type='email' />
+                                                <MDBInput className='mt-3' label='Hall Name' id='name'
+                                                    value={values.name} onChange={handleChange} type='text' />
+
+                                                <div className="row">
+                                                    <div className="col-md-6  mt-3">
+                                                        <MDBInput className='' label='Size (in sq feet)' id='size'
+                                                            value={values.size} onChange={handleChange} type='text' />
+                                                    </div>
+                                                    <div className="col-md-6 mt-3">
+                                                        <MDBInput className='' label='Rate (per hours)' id='rate' value={values.rate} onChange={handleChange} type='text' />
+                                                    </div>
+                                                </div>
+                                                <MDBTextArea className='my-3' label='Location' rows={3} id='location' value={values.location} onChange={handleChange} />
+                                                <MDBTextArea className='my-3' label='Facilities' id='facilities' value={values.facilities} onChange={handleChange} rows={4} />
+
+                                                <div className="my-3">
+                                                    <label className="form-label mx-2" for="typeText">Upload Image</label>
+                                                    <input type="file" id="" onChange={uploadFile} />
+                                                </div>
+
+                                                <button type="submit" className="btn btn-success w-100">
+                                                    Submit
+                                                </button>
+                                            </form>
+                                        )}
+
+                                    </Formik>
+
+
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
         </div>
-      </div>
-      <div className="col">
-        <div className="form-outline">
-          <input type="text" id="form6Example2" className="form-control" />
-          <label className="form-label" htmlFor="form6Example2">
-            Last name
-          </label>
-        </div>
-      </div>
-    </div>
-    {/* Text input */}
-    <div className="form-outline mb-4">
-      <input type="text" id="form6Example3" className="form-control" />
-      <label className="form-label" htmlFor="form6Example3">
-        Office Space name
-      </label>
-    </div>
-    {/* Text input */}
-    <div className="form-outline mb-4">
-      <input type="text" id="form6Example4" className="form-control" />
-      <label className="form-label" htmlFor="form6Example4">
-        Address
-      </label>
-    </div>
-    {/* Email input */}
-    <div className="form-outline mb-4">
-      <input type="email" id="form6Example5" className="form-control" />
-      <label className="form-label" htmlFor="form6Example5">
-        Email
-      </label>
-    </div>
-    {/* Number input */}
-    <div className="form-outline mb-4">
-      <input type="number" id="form6Example6" className="form-control" />
-      <label className="form-label" htmlFor="form6Example6">
-        Phone
-      </label>
-    </div>
-    {/* Message input */}
-    <div className="form-outline mb-4">
-      <textarea
-        className="form-control"
-        id="form6Example7"
-        rows={4}
-        defaultValue={""}
-      />
-      <label className="form-label" htmlFor="form6Example7">
-       Space Type:-
-      </label>
-      <div className="form-check">
-    <input
-      className="form-check-input"
-      type="checkbox"
-      defaultValue=""
-      id="flexCheckDefault"
-    />
-    <label className="form-check-label" htmlFor="flexCheckDefault">
-      Single seater
-    </label>
-  </div>
-  {/* Checked checkbox */}
-  <div className="form-check">
-    <input
-      className="form-check-input"
-      type="checkbox"
-      defaultValue=""
-      id="flexCheckChecked"
-      defaultChecked=""
-    />
-    <label className="form-check-label" htmlFor="flexCheckChecked">
-      Multiple seater
-    </label>
-  </div>
-    </div>
-    {/* Checkbox */}
-    <div className="form-check d-flex justify-content-center mb-4">
-      <input
-        className="form-check-input me-2"
-        type="checkbox"
-        defaultValue=""
-        id="form6Example8"
-        defaultChecked=""
-      />
-      <label className="form-check-label" htmlFor="form6Example8">
-        Create an account?
-      </label>
-    </div>
-    {/* Submit button */}
-    <button type="submit" className="btn btn-primary btn-block mb-4">
-      ADD SPACE
-    </button>
-  </form>
-  
-  )
+    )
 }
 
-export default AddSpace;
+export default AddSpace
