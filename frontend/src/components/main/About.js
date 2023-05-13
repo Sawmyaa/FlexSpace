1e9ef7adfadb926f298a2fdf0fd1c2777bda8c80
Kajal Madhesiya <madheshiyakajal@gmail.com>
2023-05-13 15:25:33 +0530

    Update About.js

--- frontend/src/components/main/About.js
+++ frontend/src/components/main/About.js
@@ -3,55 +3,56 @@ import React from 'react'
 const About = () => {
   return (
     <>
-      <section className="about-section" style={{ backgroundImage: 'url("/logo3.jpg")' }}>
-        <div className="inner-container">
-          <h1 className="about">FlexSpace</h1>
-          <p className="text">
-            FlexSpace provides a valuable  service to users looking for  temporary office space and helps  office space providers manage their spaces efficiently.<br />
-            From a user-friendly interface to automated processes and cost-saving features , our platform offers a unique and efficient experience for both customers and business owners. We provide variety of prices, facilities, location at one places.
-          </p>
+     <section className="about-section" style={{backgroundImage:'url("/logo3.jpg")'}}>
+      <div className="inner-container">
+        <h1 className="about">FlexSpace</h1>
+        <p className="text">
+        FlexSpace provides a valuable  service to users looking for  temporary office space and helps  office space providers manage their spaces efficiently.<br/>
+        From a user-friendly interface to automated processes and cost-saving features , our platform offers a unique and efficient experience for both customers and business owners. We provide variety of prices, facilities, location at one places.
+        </p>
         </div>
-
-
+      
+    
       </section>
+      <div className='about' style={{backgroundColor:" rgb(216, 208, 199", border: "2px"}}>
       <div className='about-section1'>
-        <div className="content-box-lg">
-          <div className="container">
-            <div className='row'>
-              <div className="col-md-4">
-                <div className="about-item text-center">
-                  <i className="fa fa-globe"></i>
-                  <h3>Location</h3><hr />
-                  <p>: Users can choose from a wide range  of available office spaces, including different  locations, sizes, and amenities, allowing them  to find the space that best suits their needs.
-                  </p>
+       <div className="content-box-lg">
+        <div className="container">
+        
+          <div className='row'>
+          <div className="col-md-4">
+            <div className="about-item text-center" style={{height:"248px"}}>
+              <i className="fa fa-globe"></i>
+              <h3>Location</h3><hr/>
+              <p>Users can choose from a wide range  of available office spaces, including different  locations, sizes, and amenities, allowing them  to find the space that best suits their needs.
+</p>
 
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="about-item text-center">
-                  <i className="fa fa-search"></i>
-                  <h3>Transparency</h3><hr />
-                  <p>Users can view detailed information about available office spaces,  including photos, reviews, and pricing  information, allowing them to make informed  decisions about where to book.
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="about-item text-center" style={{height:"248px"}}>
+            <i className="fa fa-search"></i>
+           <h3>Transparency</h3><hr/>
+              <p>Users can view detailed information about available office spaces,  including photos, reviews, and pricing  information, allowing them to make informed  decisions about where to book.
 
-                  </p>
+</p>
 
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="about-item text-center" style={{ height: "248px" }}>
-                  <i className="fa fa-location"></i>
-                  <h3>Flexibility</h3><hr />
-                  <p>Users can book office spaces for a  shorter duration of time, making it easier for  them to adapt to changing business needs
-                  </p>
-                </div>
-              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="about-item text-center" style={{height:"248px"}}>
+              <i className="fa fa-location"></i>
+              <h3>Flexibility</h3><hr/>
+              <p>Users can book office spaces for a  shorter duration of time, making it easier for  them to adapt to changing business needs
+</p>
+</div>
             </div>
           </div>
         </div>
+       </div>
       </div>
-
-
-
+      </div>
+     
     </>
   )
 }
