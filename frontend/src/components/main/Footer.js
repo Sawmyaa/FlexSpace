b26c0c9f39fb65d007d023cbc3b379c00ea0e59d
madheshiyakajal <117758679+madheshiyakajal@users.noreply.github.com>
2023-05-19 22:22:04 +0530

    Update Footer.js

--- frontend/src/components/main/Footer.js
+++ frontend/src/components/main/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-<footer className="bg-dark text-center text-white">
+<Footer className="bg-dark text-center text-white">
   {/* Grid container */}
   <div className="container p-4 pb-0">
     {/* Section: Social media */}
@@ -71,7 +71,7 @@ const Footer = () => {
     </a>
   </div>
   {/* Copyright */}
-</footer>
+</Footer>
 
 
     </div>
