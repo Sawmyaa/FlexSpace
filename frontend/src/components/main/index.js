4f75e3fdf65a7136c26642d7c63d67d0e073e024
Kajal Madhesiya <madheshiyakajal@gmail.com>
2023-05-20 13:08:33 +0530

    ..

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -3,11 +3,8 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer'
 
-
-
-
-
 const Main = () => {
+  console.log('sakdjhj');
   return (
     <div>
       <Navbar/>
