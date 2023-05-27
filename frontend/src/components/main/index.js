cd604c99bcd249791dd74ae531328aa19efd8782
Saumya Maurya <118256107+Sawmyaa@users.noreply.github.com>
2023-05-27 15:46:21 +0530

    ..

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
-import Footer from './footer'
+import Footer from './Footer'
 
 const Main = () => {
   console.log('sakdjhj');
