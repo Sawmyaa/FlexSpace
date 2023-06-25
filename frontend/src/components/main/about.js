889419d6eb9965e0f5b0e375f343aee5842414fd
Kajal Madhesiya <madheshiyakajal@gmail.com>
2023-06-25 14:07:00 +0530

    ..

--- frontend/src/components/main/about.js
+++ frontend/src/components/main/about.js
@@ -8,7 +8,7 @@ const About = () => {
         <h1 className="about">FlexSpace</h1>
         <p className="text">
         FlexSpace provides a valuable  service to users looking for  temporary office space and helps  office space providers manage their spaces efficiently.<br/>
-        From a user-friendly interface to automated processes and cost-saving features , our platform offers a unique and efficient experience for both customers and business owners. We provide variety of prices, facilities, location at one places.
+        From a user-friendly interface to automated processes and cost-saving features, our platform offers a unique and efficient experience for both customers and business owners. We provide variety of prices, facilities, location at one places.
         </p>
         </div>
       
@@ -51,7 +51,38 @@ const About = () => {
         </div>
        </div>
       </div>
-      </div>
+      </div><br/><br/>
+      <div className='contact' style={{background:"bluish"}}>
+        <h2 className="common-heading text-center" style={{fontWeight: 600, color:"Blue"}}>Feels Free to Contact us</h2><br/><br/>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.4650675136445!2d81.05642547456617!3d26.888731661122314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399be209f58895a1%3A0x8328acaa3eddd4f!2z4Kas4Ka-4Kas4KeBIOCmrOCmqOCmvuCmsOCmuOCmvyDgpqbgpr7gprgg4KaH4KaJ4Kao4Ka_4Kat4Ka-4Kaw4KeN4Ka44Ka_4Kaf4Ka_!5e0!3m2!1sbn!2sin!4v1687538838304!5m2!1sbn!2sin" 
+        width="100%" height="350" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+ </iframe>
+ <div className='container mt-0'style={{background:"whitesmoke", height:"100%", width:"70%"}}>
+    <form action="#" method="POST">
+      <input type="text"
+      name="username"
+      placeholder='username'
+      className="form-control form-control-lg"
+      autoComplete='off'
+      required/><br/>
+      
+      <input type="email"
+      name="Email"
+      placeholder='Email'
+      className="form-control form-control-lg"
+      autoComplete='off'
+      required/><br/>
+      <textarea 
+      name="message" 
+      cols="109" 
+      rows="5"
+      placeholder=' Write Something...'
+      autoComplete='off required'></textarea><br/>
+      <button type="submit"style={{background:"blue", color:"white", borderRadius:"5px", padding:"6px", border:"1px Blue"}}>Send Message</button>
+    </form>
+  </div>
+ </div>
+    
      
     </>
   )
